d98bff4d5a76f3724fb4e50671f96a12a46c2511
lgisler <48827180+lgisler@users.noreply.github.com>
2025-07-02 16:26:21 +0200

    add additional python checks

--- pre-commit-hooks/sort-cspell-words.py
+++ pre-commit-hooks/sort-cspell-words.py
@@ -2,6 +2,7 @@
 import sys
 import yaml
 
+
 def sort_and_deduplicate_words(words):
     seen = set()
     result = []
@@ -13,10 +14,11 @@ def sort_and_deduplicate_words(words):
     # Now sort by lower-case, but keep original case
     return sorted(result, key=lambda w: w.lower())
 
+
 for fname in sys.argv[1:]:
     with open(fname) as f:
         data = yaml.safe_load(f)
-    if 'words' in data and isinstance(data['words'], list):
-        data['words'] = sort_and_deduplicate_words(data['words'])
-        with open(fname, 'w') as f:
+    if "words" in data and isinstance(data["words"], list):
+        data["words"] = sort_and_deduplicate_words(data["words"])
+        with open(fname, "w") as f:
             yaml.dump(data, f, sort_keys=False, allow_unicode=True)
